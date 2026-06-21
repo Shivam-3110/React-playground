ade2929de829ee4d791fd1f6aa609832eb6eb675
Shivam-3110 <mrshivam00022@gmail.com>
2026-06-21 18:40:40 +0530

    Wrapper and Context API

---
+++ DAY4/src/components/Wrapper.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+function Wrapper(props) {
+    console.log(props)
+  return   props.App;
+   
+  
+}
+
+export default Wrapper
--- DAY4/src/main.jsx
+++ DAY4/src/main.jsx
@@ -4,9 +4,11 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import App from './App.jsx'
 import './index.css'
+import Wrapper from './components/Wrapper.jsx';
 createRoot(document.getElementById('root')).render(
-  <>
+/*  <>
     <App />
     <ToastContainer position="top-center"/>
- </>
+ </> */
+  <Wrapper  App ={<App/>}/>
 )
