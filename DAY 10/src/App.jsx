02817cf06984bd02df3039f4cf9a9bcc05aa4c16
Shivam-3110 <mrshivam00022@gmail.com>
2026-09-09 16:21:19 +0530

    "form validation performed"

--- DAY 10/src/App.jsx
+++ DAY 10/src/App.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import Usercard from './components/usercard'
 import Form from './components/Form'
 
 export default function App() {
   const [toggle,setToggle] = useState(false);
+   const[users,setUsers] = useState([])
   return (
-    <div className = "p-3 h-screen ">
+    <div className = "p-3 h-screen flex flex-col gap-4">
       <Navbar/>
       {
         toggle?<div>
        <Usercard/>
-      </div>: <div>
-        <Form/>
+      </div>: <div className ="flex justify-center items-center h-[70%]">
+        <Form setUsers={setUsers}/>
       </div>
       }
     </div>
--- DAY 10/src/components/Form.jsx
+++ DAY 10/src/components/Form.jsx
@@ -1,22 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {useForm} from 'react-hook-form';
+function Form ({setUsers}) {
 
-function Form () {
+  let {register,handleSubmit,reset,formState:{errors}} = useForm({ mode:"onChange"});
+  
+  let formSubmit = (data) => {
+    console.log(data);
+    setUsers((prev) => [...prev,data])
+    reset();
+  }
   return (
-    <div>
-        <h1> Create User </h1>
-        <form>
-            <input type="text"
-             placeholder='enter the name' name="name"/>
-             <input type="email"
+    <div className ="flex flex-col gap-6">
+        <h1 className="text-xl font-semibold"> Create User </h1>
+        <form  onSubmit={handleSubmit(formSubmit)} className ="w-60 flex flex-col bg-black gap-3 p-4 rounded border-white">
+             
+              <input {...register("name",{required:"Name is mandatory"})} className ='p-2 rounded text-white outline-0 border border-white' type="text"
+             placeholder="enter the name" name="name"/>
+               {errors.name && (
+          <p className="text-red-500">
+            {errors.name.message}
+          </p>
+        )}
+             <input {...register("email",{required:"email is mandatory"})} className="p-2 rounded text-white outline-0 border border-white" type="email"
              placeholder="enter the email" name="email"/>
-              <input type="Number"
+              {errors.email && ( <p className='text-red-500'>  {errors.email.message} </p>)}
+              <input {...register("mobile",{required:"mobile is mandatory"})} className="p-2 rounded text-white outline-0 border border-white" type="Number"
              placeholder="enter the mobile No" name="mobile"/>
-              <input type="url"
+             {errors.mobile &&( <p className='text-red-500'>  {errors.mobile.message} </p>)}
+              <input {...register("image",{required:"image is mandatory"})} className="p-2 rounded text-white outline-0 border border-white" type="url"
              placeholder="enter the image url" name="image"/>
-             <button> Add user </button>
+             {errors.image && (<p className='text-red-500'>  {errors.image.message} </p>)}
+             <button className='text-white bg-blue-700 p-4 rounded cursor-pointer'> Add user </button>
         </form>
-    </div>
+    </div>  
   )
 }
-
 export default Form;
